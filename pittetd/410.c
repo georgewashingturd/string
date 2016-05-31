4696fed76c71b9e930e58309eb40db983dde557f
georgewashingturd <pittclasses@gmail.com>
2016-05-30 21:52:38 -0700

    Update 46

--- pittetd/410.c
+++ pittetd/410.c
@@ -3,5 +3,16 @@
 
 int main (void)
 {
+    unsigned long long ku, kd, w, a;
+    
+    for (w = 1ULL; w < 10LL; w++)
+    {
+        ku = (1ULL*w*w)-(5ULL*w)+8ULL;
+        kd = 10ULL;
+        printf("%lld %lld 30 : %lld\n", (1ULL*w*w) % kd, -((5ULL*w) % kd), ku % kd);
+    }
+    
     return 0;
+    
+    
 }
