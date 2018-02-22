e240605d30b2051556b89ae535f7bcaf6fa1e4fb
georgewashingturd <pittclasses@gmail.com>
2018-02-21 21:29:17 -0800

    Update

--- pittetd/cycAll.py
+++ pittetd/cycAll.py
@@ -67,6 +67,7 @@ def cycNorm(f):
 
 import sys
 
+# the following two functions are used to generate primitive roots
 # translated to Python from http://www.bluetulip.org/2014/programs/primitive.js
 # (some rights may remain with the author of the above javascript code)
 
@@ -94,7 +95,40 @@ def primRoots(theNum):
         o = 1
         r = r + 1
     return roots
+    min_tot = 2 + (L-1)/2
+    min_e = 2
 
+# search a factor e of L-1 such that e+f is minimized
+
+def searchMinE(L):
+    # we know we are searching a parabola and sqrt(L-1)
+    # is the minimum point thus we break once we find the closest
+    # integer factor but we search backwards and forwards
+
+    if L <= 1:
+        return L
+
+    min_tot = 2 + (L-1)/2
+    min_e = 2
+    
+    s = int(sqrt(L-1))
+    for i in xrange(s,L):
+        if ((L-1)% i) == 0:
+            if (i + (L-1)/i) < min_tot:
+                min_e = i
+                min_tot = i + (L-1)/i
+            break
+
+    for i in xrange(s,0,-1):
+        if ((L-1)% i) == 0:
+            if (i + (L-1)/i) < min_tot:
+                min_e = i
+                min_tot = i + (L-1)/i
+            break
+
+    return min_e
+
+# funtion to do sigma conjugation
 def sigmaShift(f, g, k):
     L = len(f)
     return cycShift(f, pow(g,k,L))
@@ -103,7 +137,7 @@ def sigmaShift(f, g, k):
 # total e + f multiplications rather than e*f
 # but first you must find a primitive root so the total
 # time needed might be the same
-def cycNormFast(f, g):
+def cycNormFast(f, g, e=2):
 
     # Find primitive a root
     # Create the \sigma operator \alpha \to \alpha^\gamma
@@ -117,7 +151,7 @@ def cycNormFast(f, g):
     #g = primRoots(L)[0]
 
     # for now set e = 2
-    e = 2
+    # e = 2
     F = (L-1)/e
 
     # now construct G
@@ -172,7 +206,21 @@ def divCyc(f, h):
 
     return res
 
-# calculate prime factor
+# find factors of a number to be used for generating periods
+from math import sqrt
+def factors(n):    # (cf. https://stackoverflow.com/a/15703327/849891)
+    j = 2
+    while n > 1:
+        for i in xrange(j, int(sqrt(n+0.05)) + 1):
+            if n % i == 0:
+                n /= i ; j = i
+                yield i
+                break
+        else:
+            if n > 1:
+                yield n; break
+        
+# calculate prime factor for primes that divide a binomial
 def binPrimeFactor(L, p):
     if (p-1) % L != 0:
         return
@@ -188,27 +236,88 @@ def binPrimeFactor(L, p):
 
     # prepare things for cycNormFast
     g = primRoots(L)[0]
+
+    # this part is to find e and f that minimizes e + f
+    # so that we minimize the number of multiplication
+    # in the cycNormFast function
     
+    min_e = searchMinE(L)
+    print min_e
+
+    # now we generate all combos for powers of k
     import itertools
     t = [i for i in range(1,L)]
     for u in xrange(2,L):
         
-        l = list(itertools.combinations(t, u))
+        l = itertools.combinations(t, u)
 
         for i in l:
             # create a bitmap for the plus minus
             for k in range(0,2**u):
                 res = [0] * L
                 for j in range(u):
-                    b = (k >> j) & 0x01
-                    if b == 0:
-                        b = -1
+                    b = pow(-1,(k >> j) & 0x01)
                     res[i[j]] = b*1
                     res[0] -= b*kp[i[j]]
-                N = cycNormFast(res, g)
+                N = cycNormFast(f=res, g=g, e=min_e)
                 if N == p:
+                    print N, res, u
+
+def findSquarePrime(L, a, p):
+    
+    hl = (((L-1)/2) + 1)
+    kp = [1] * hl
+    for i in xrange(1,hl):
+        kp[i] = (pow(a,i,p) + pow(a,L-i,p)) % p
+
+    print kp
+
+    g = primRoots(L)[0]
+    min_e = searchMinE(L)
+    
+    import itertools
+    t = [i for i in range(1,hl)]
+    
+    for u in xrange(2,hl):
+        
+        l = itertools.combinations(t, u)
+
+        for i in l:
+            # create a bitmap for the plus minus
+            for k in range(0,2**u):
+                res = [0] * L
+                for j in range(u):
+                    b = pow(-1,(k >> j) & 0x01)
+                    res[i[j]] = b*1
+                    res[L-i[j]] = b*1
+                    res[0] -= 2*b*kp[i[j]]
+                N = cycNormFast(f=res, g=g, e=min_e)
+                if N == p**2:
                     print N, res, u
 
 
 
-    
+# calcule f(k) mod p given f(\alpha)
+def calcFK(f, k, p):
+    L = len(f)
+
+    res = 0
+    for i in range(L):
+        res += f[i]*pow(k,i,p)
+
+    return res % p
+
+# find if a conjugate of f(\alpha) satisfies f(k) \equiv 0 \pmod{p}
+def findConjWithCorrectK(f, k, p):
+
+    L = len(f)
+    for i in xrange(1, L):
+        if calcFK(cycShift(f, i), k, p) == 0:
+            print i
+
+
+
+
+
+
+        
